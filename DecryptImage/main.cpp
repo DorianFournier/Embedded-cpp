5f2c2dd4df4297daf2abd321c65b51a864943e91
DorianFournier <dorian.fournier@ynov.com>
2023-04-22 15:15:28 +0200

    modify ui

--- DecryptImage/main.cpp
+++ DecryptImage/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Decrypt Image");
+    w.setWindowTitle("Decrypt Message");
+    w.setFixedSize(644, 314);
     w.show();
     return a.exec();
 }
--- DecryptImage/mainwindow.cpp
+++ DecryptImage/mainwindow.cpp
@@ -1,12 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QGraphicsObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     ui->decryptButton->setDisabled(true);
     decrypt_process = new DecryptProcess();
 }
@@ -21,7 +23,8 @@ void MainWindow::chooseFile(void)
     _hidden_file_name = QFileDialog::getOpenFileName(this, tr("Open File"),
                                                     _initial_directory,
                                                     tr("Images (*.png)"));
-
+    ui->textBrowser->setText("No file decrypted yet !");
+    ui->textBrowser->setStyleSheet("QTextBrowser { font-style: italic; color: #999999; }");
     QRegularExpression pattern("[^/]+$");
     QRegularExpressionMatch match = pattern.match(_hidden_file_name);
     if (match.hasMatch()) {
@@ -36,4 +39,5 @@ void MainWindow::decryptFile(void)
     _hidden_message = decrypt_process->showData(_hidden_file_name);
     qDebug() << "Hidden word is : " << _hidden_message<<"";
     ui->textBrowser->setText(_hidden_message);
+    ui->textBrowser->setStyleSheet("QTextBrowser {color: #000000; }");
 }
